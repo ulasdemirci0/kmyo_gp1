7c539a5ebc5f8d40e5e5a2e939182a8a2e52e1e9
Ulaş Demirci <44036168+ulasdemirci0@users.noreply.github.com>
2021-04-05 23:22:21 +0300

    Soru 8

---
+++ odev_soru_8/odev_soru_8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Timers;
+
+namespace odev_soru_8
+{
+    class Program
+    {
+        public Timer timer = new Timer();
+        public int elapsed = 0;
+        void timerSetup()
+        {
+            elapsed = 0;
+            if (timer.Enabled)
+                timer.Stop();
+            timer.Interval = 500;
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            elapsed++;
+        }
+
+        static void Main(string[] args)
+        {
+            Program prg = new Program();
+            prg.timerSetup();
+            prg.timer.Start();
+            while (prg.timer.Enabled)
+            {
+                Console.Clear();
+                Console.WriteLine("Kronometre: " + prg.elapsed);
+                System.Threading.Thread.Sleep(500);
+            }
+
+        }
+    }
+}
